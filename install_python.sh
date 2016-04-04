05a7862351a9f13dc4f35262ceb7e9dfbda081d1
Matthew Rubashkin <mrubashkin@svds.com>
2016-04-04 16:38:06 -0700

    update install_python.sh to use python 2.7.9

--- install_python.sh
+++ install_python.sh
@@ -11,7 +11,7 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 #Python 3 version to install
-version="3.5.1"
+version="2.7.9"
 
 #------------------------------------------------------
 apt-get install -y build-essential libncursesw5-dev
